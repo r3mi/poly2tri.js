413cc84e2cb71e1ed5d7997bec2622fca67cb43a
Rémi <r3mi@users.sourceforge.net>
2014-01-15 22:28:12 +0100

    benchmark.js : add spinner

--- tests/benchmark.js
+++ tests/benchmark.js
@@ -1,45 +1,17 @@
 #!/usr/bin/env node
 /*
- * Poly2Tri Copyright (c) 2009-2013, Poly2Tri Contributors
- * http://code.google.com/p/poly2tri/
- * 
- * poly2tri.js (JavaScript port) (c) 2009-2013, Poly2Tri Contributors
- * https://github.com/r3mi/poly2tri.js
- *
  * Benchmarking tests for poly2tri.js
- * Rémi Turboult, 12/2013
- *
+ * 
+ * (c) 2013-014, Rémi Turboult
  * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * * Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- * * Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- * * Neither the name of Poly2Tri nor the names of its contributors may be
- *   used to endorse or promote products derived from this software without specific
- *   prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * Distributed under the 3-clause BSD License, see LICENSE.txt
  */
 
 "use strict";
 
 var Benchmark = require('benchmark').Benchmark;
 var load = require('load');
+var Linespin = require('linespin');
 
 var suite = new Benchmark.Suite();
 
@@ -134,8 +106,21 @@ function triangulate(P) {
 versions.forEach(function(v) {
     try {
         var poly2tri = loaders[v].call();
+        var spin;
         suite.add(v, function() {
             triangulate(poly2tri);
+        }, {
+            onStart: function() {
+                spin = new Linespin(v);
+                spin.start();
+            },
+            onError: function(event) {
+                spin.error(event.message);
+            },
+            onComplete: function(event) {
+                spin.doneMessage = String(event.target);
+                spin.stop();
+            }
         });
         console.log("Loaded version " + v);
     } catch (e) {
@@ -148,13 +133,9 @@ versions.forEach(function(v) {
 /*
  * Run tests
  */
-console.log("Benchmarking ...");
 
 // add listeners
 suite
-        .on('cycle', function(event) {
-    console.log(String(event.target));
-})
         .on('complete', function() {
     console.log("");
     console.log('Fastest is ' + this.filter('fastest').pluck('name'));
